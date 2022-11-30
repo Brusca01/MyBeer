1fc1b9a77e5f9a01f53413e890bdf315b0016461
Brusca01 <84087640+Brusca01@users.noreply.github.com>
2022-11-30 19:14:46 +0100

    set icon - rimozione titolo

--- app/src/main/java/com/uninsubria/mybeer/SplashScreenActivity.kt
+++ app/src/main/java/com/uninsubria/mybeer/SplashScreenActivity.kt
@@ -7,11 +7,13 @@ import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        getSupportActionBar()?.hide()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         iv_note.alpha = 0f
-        iv_note.animate().setDuration(1500).alpha(1f).withEndAction{
+        iv_note.animate().setDuration(2000).alpha(1f).withEndAction{
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
